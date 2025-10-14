6ae60cb068dcca53f6e86a52719403ba45cbed0a
rafael_bertao <rafaelbertaodesousa123@gmail.com>
2025-10-14 10:17:14 -0300

    Separado server e client em .cpp e .hpp

--- Parte2HAE/aula2/client.cpp
+++ Parte2HAE/aula2/client.cpp
@@ -1,29 +1,17 @@
-//projeto.hpp (continuacao)
-class CLIENT: public DEVICE {
-    const string PORT="3490"; // the port client will be connecting to
-    int sockfd, numbytes;
-    ...
-    static void *get_in_addr(struct sockaddr *sa) {
-        //Esta funcao esta repetida em SERVER e CLIENT.
-        //E’ possivel unificar usando DEVICE.
-        ...
-    }
+#include "client.hpp"
 
-public:
-    CLIENT(string endereco) {
-        memset(&hints, 0, sizeof hints);
-        ...
-    }
-    
-    ~CLIENT() {
-        close(sockfd);
-    }
+CLIENT::CLIENT(string endereco) {
 
-    void sendBytes(int nBytesToSend, BYTE *buf) {
-        ...
-    }
+}
 
-    void receiveBytes(int nBytesToReceive, BYTE *buf) {
-        ...
-    }
-}; //Fim de CLIENT
+CLIENT::~CLIENT() {
+
+}
+
+void CLIENT::sendBytes(int nBytesToSend, BYTE *buf) {
+
+}
+
+void CLIENT::receiveBytes(int nBytesToReceive, BYTE *buf) {
+
+}
--- Parte2HAE/aula2/server.cpp
+++ Parte2HAE/aula2/server.cpp
@@ -2,39 +2,24 @@
 #include "raspberry.hpp"
 #include <unistd.h>
 
-//...
+#include "./server.hpp"
 
-class SERVER {
-    const string PORT="3490"; // the port users will be connecting to
-    const int BACKLOG=1; // how many pending connections queue will hold
-    int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
+void SERVER::SERVER() {
     
-    struct addrinfo hints, *servinfo, *p;
-...
-    static void *get_in_addr(struct sockaddr *sa) {
-    //Esta funcao esta repetida em SERVER e CLIENT.
-    //E’ possivel unificar usando DEVICE.
-    ...
-    }
+}
 
-public:
-    SERVER() {
-        memset(&hints, 0, sizeof hints);
-        //...
-    }
-    ~SERVER() {
-        close( ...
-    }
+void SERVER::~SERVER() {
 
-    void waitConnection() {
-        printf("server: waiting for connections...\n");
-        while (1) {
-    ...
-    }
-    void sendBytes(int nBytesToSend, BYTE *buf) {
-    ...
-    }
-    void receiveBytes(int nBytesToReceive, BYTE *buf) {
-    ...
-    }
-}; //Fim de SERVER
+}
+
+void SERVER::waitConnection() {
+
+}
+
+void SERVER::sendBytes(int nBytesToSend, BYTE *buf) {
+
+}
+
+void SERVER::receiveBytes(int nBytesToReceive, BYTE *buf) {
+
+}
